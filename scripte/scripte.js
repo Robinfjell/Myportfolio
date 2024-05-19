7bf864c0b429ae85a263758ad3e8bb7f8d0798e3
Robinfjell <rg5632256@gmail.com>
2024-05-20 01:03:15 +0200

    portfolio
    Since I'm just starting out, I have a lot of mistakes, unfortunately I couldn't fix some of them, for example, the form appears above the navigation bar. Secondly, the video I set to autoplay, which needs to be repeated every 5 seconds, does not work properly. Also, when I hover over the boxes, I wanted the boxes to grow, but only the text gets bigger. And I didn't like the last footer part, but that's okay, this is just the first try. I'm sure I will write a much better project, good luck to everyone.

---
+++ scripte/scripte.js
@@ -0,0 +1,7 @@
+
+        // 5 saniye sonra videoyu otomatik olarak başlat
+        setTimeout(function() {
+            var video = document.getElementById('myVideo');
+            video.play();
+        }, 5000); // 5000 milisaniye = 5 saniye
+   
